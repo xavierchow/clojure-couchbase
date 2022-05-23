698e4cfc0e201bc10e816d8d9e4a92892ddfdd0b
Xavier Zhou <xiayezhou@gmail.com>
2022-05-24 00:14:52 +0800

    chore: remove unused test code

--- test/clojure_couchbase/core_test.clj
+++
@@ -1,9 +0,0 @@
-(ns clojure-couchbase.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-couchbase.core :refer :all]))
-
-(deftest a-test
-  (testing "SUCCESS"
-    (is (= 1 1)))
-  (testing "SUCCESS 2"
-    (is (= "a" "a"))))
